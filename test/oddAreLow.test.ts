ef4a656137a6f998cc4c4f68b3b0aebd5055b542
rcmtcristian <rcmtcristian@gmail.com>
2023-11-26 20:16:41 -0500

    chore(index): removed some comments

--- test/oddAreLow.test.ts
+++ test/oddAreLow.test.ts
@@ -17,27 +17,4 @@ suite("oddsAreLow Function Tests", () => {
   });
 
 
-  
-  // // The function should return the string "Odds are high!" when the input is a truthy value other than true.
-
-  // test('returns "Odds are high!" when isLow is a truthy value other than true', () => {
-  //   const result = oddsAreLow(1);
-  //   return result === "Odds are high!";
-  // });
-
-  // // The function should return the string "Odds are low!" when the input is a falsy value other than false.
-
-  // // Test case 5
-  // test('returns "Odds are low!" when isLow is a falsy value other than false', () => {
-  //   const result = oddsAreLow(0);
-  //   return result === "Odds are low!";
-  // });
-
-  // // The function should return the string "Odds are high!" when the input is not a boolean value.
-
-  // // Test case 6
-  // test('returns "Odds are high!" when isLow is not a boolean value', () => {
-  //   const result = oddsAreLow("true");
-  //   return result === "Odds are high!";
-  // });
 });
